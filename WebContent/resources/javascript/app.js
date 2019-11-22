348c5fe0605a5f5c55d2af0110db91aeb19de9a8
Diogo <dramoslopesilveira@gmail.com>
2019-11-22 17:19:25 -0300

    alterações e atualização da pagina 404

--- WebContent/resources/javascript/app.js
+++ WebContent/resources/javascript/app.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	$('.js-toggle-menu').bind('click', function(){
 		$('.js-sidebar').toggleClass('is-toggle');
 		$('.js-content').toggleClass('is-toggle');
+		$('.js-rodape').toggleClass('is-toggle');
 	});
 	
 });
