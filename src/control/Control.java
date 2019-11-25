ea49f609a3a7c67c0c4ea330d4d30c558c7d54f9
Diogo <dramoslopesilveira@gmail.com>
2019-11-25 09:50:46 -0300

    pagina acesso negado e erro 404 encaminhadas

--- src/control/Control.java
+++ src/control/Control.java
@@ -1,5 +1,14 @@
 package control;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "control")
+@SessionScoped
 public class Control {
+	
+	public String login() {
+		return "login";
+	}
 
 }
